6d77c1b67ede548e46bd5cbc7561cf850d29843b
euside <eugeside@gmail.com>
2023-01-11 16:23:17 +0200

    change path and add exact param

--- src/JS/App.js
+++ src/JS/App.js
@@ -25,7 +25,7 @@ const App = () => {
             <Layout />
             <div id="mainWraperId">
                 <Routes>
-                    <Route path="/" index element={<Home />}></Route>
+                    <Route path="/euside" index exact element={<Home />}></Route>
                     <Route path="/project" element={<Project />}></Route>
                     <Route path="/about" element={<About />}></Route>
                     <Route path="/contact" element={<Contact />}></Route>

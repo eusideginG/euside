5604b1550ecc7f0904f91e49001e0a17f5fe009e
euside <eugeside@gmail.com>
2023-01-11 14:23:49 +0200

    Change BrowserRouter to HushRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"
+import { HushRouter } from "react-router-dom"
 import "./index.css";
 import App from "./JS/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter>
+    <HushRouter>
         <App />
-    </BrowserRouter>
+    </HushRouter>
 );
